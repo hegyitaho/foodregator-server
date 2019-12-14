875e05e089ee6336b6af321816332eb8aa4d630d
hegyitaho <hegyitaho@gmail.com>
2019-12-14 22:58:06 +0100

    validate schema

--- test/app.e2e.spec.ts
+++ test/app.e2e.spec.ts
@@ -1,6 +1,8 @@
 import {Test, TestingModule} from '@nestjs/testing'
 import request from 'supertest'
-import {AppModule} from './../src/app.module'
+import Joi from '@hapi/joi'
+import {AppModule} from 'src/app.module'
+import { FoodType } from 'src/scraping/beresalexandra/utils/conversion'
 
 describe('AppController (e2e)', () => {
   let app
@@ -14,10 +16,28 @@ describe('AppController (e2e)', () => {
     await app.init()
   })
 
-  it.skip('beresalexandra returns food data', () => {
+  it('beresalexandra returns food data', () => {
     return request(app.getHttpServer())
       .get('/beresalexandra')
       .expect(200)
-      .then(({body}) => expect(body).toEqual([]))
+      .then(({body}) => responseSchema.validateAsync(body))
   })
 })
+
+
+const foodDataSchema = Joi.object({
+    carbohydrate: Joi.number().required(),
+    date: Joi.date().required(),
+    fat: Joi.number().required(),
+    kcal: Joi.number().required(),
+    name: Joi.string().required(),
+    price: Joi.number().required(), 
+    protein: Joi.number().required(),
+    type: Joi.valid(...Object.values(FoodType))
+  })
+
+const responseSchema = Joi.array().items(foodDataSchema)
+
+/*
+{'carbohydrate': '21', 'date': new Date(2019, 4, 27), 'fat': '3', 'kcal': '154', 'name': 'Parajfőzelék', 'price': '580', 'protein': '10', 'type': 'FOZELEK'},
+*/
